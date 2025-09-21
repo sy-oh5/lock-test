94454022b524b82a2da1665da3acae1b0db0955e
snggooroid <>
2025-09-21 19:24:42 +0900

    비관적락

--- src/main/java/org/example/locktest/book/PessimisticLockService.java
+++ src/main/java/org/example/locktest/book/PessimisticLockService.java
@@ -1,37 +1,19 @@
 package org.example.locktest.book;
 
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+@RequiredArgsConstructor
 @Service
 public class PessimisticLockService {
 
     private final BookRepository bookRepository;
 
-    public PessimisticLockService(BookRepository bookRepository) {
-        this.bookRepository = bookRepository;
-    }
 
-    // Acquire PESSIMISTIC_WRITE lock and hold it for 3 seconds
     @Transactional
-    public void lockReadAndSleep(Long id) {
+    public void lock(Long id) throws InterruptedException {
         bookRepository.findByIdForUpdate(id).orElseThrow();
-        sleep3s();
-    }
-
-    // Try to acquire PESSIMISTIC_WRITE lock and update title
-    @Transactional
-    public void tryAcquireAndUpdateTitle(Long id) {
-        Book book = bookRepository.findByIdForUpdate(id).orElseThrow();
-        book.setTitle(book.getTitle() + "!");
-        bookRepository.flush();
-    }
-
-    private void sleep3s() {
-        try {
-            Thread.sleep(3000);
-        } catch (InterruptedException e) {
-            Thread.currentThread().interrupt();
-        }
+        Thread.sleep(3000);
     }
 }
